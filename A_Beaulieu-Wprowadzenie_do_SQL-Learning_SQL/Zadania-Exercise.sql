ec34b8cb341ca38929fd742fc7a36db61be0e075
wyderka-j <131911420+wyderka-j@users.noreply.github.com>
2023-04-26 22:04:05 +0200

    Update Zadania-Exercise.sql

--- A_Beaulieu-Wprowadzenie_do_SQL-Learning_SQL/Zadania-Exercise.sql
+++ A_Beaulieu-Wprowadzenie_do_SQL-Learning_SQL/Zadania-Exercise.sql
@@ -49,7 +49,7 @@ WHERE date(rental_date) = '2005-07-05';
 
 SELECT c.email, r.return_date
 FROM customer c 
-    INNER JOIN rental r
+INNER JOIN rental r
     ON c.customer_id = r.customer_id
 WHERE date(r.rental_date) = '2005-06-14'
 ORDER BY r.return_date DESC;
