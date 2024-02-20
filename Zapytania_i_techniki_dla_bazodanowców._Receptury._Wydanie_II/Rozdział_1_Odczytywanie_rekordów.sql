1b64a9417eb0f6d14d8c0ea43325a2ca498b4717
wyderka-j <131911420+wyderka-j@users.noreply.github.com>
2024-02-20 12:46:51 +0100

    Update Rozdział_1_Odczytywanie_rekordów.sql

--- Zapytania_i_techniki_dla_bazodanowców._Receptury._Wydanie_II/Rozdział_1_Odczytywanie_rekordów.sql
+++ Zapytania_i_techniki_dla_bazodanowców._Receptury._Wydanie_II/Rozdział_1_Odczytywanie_rekordów.sql
@@ -1,53 +1,53 @@
 -- Mircosoft SQL Server
 
---Rozdzia³ 1. Odczytywanie rekordów
+--RozdziaÅ‚ 1. Odczytywanie rekordÃ³w
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 --1.1. Odczytywanie wszystkich wierszy i kolumn tabeli
--- Chcemy zapoznaæ siê ze wszystkimi zawartymi danymi w tabeli
+-- Chcemy zapoznaÄ‡ siÄ™ ze wszystkimi zawartymi danymi w tabeli
 SELECT *
 FROM emp;
 
 --1.2. Odczytywanie podzbioru wierszy tabeli
--- Chcemy wyœwietliæ tylko te wiersze, które spe³niaj¹ okleœlony warunek
+-- Chcemy wyÅ›wietliÄ‡ tylko te wiersze, ktÃ³re speÅ‚niajÄ… okleÅ›lony warunek
 SELECT *
 FROM emp
 WHERE deptno =10;
 
---1.3. Odnajdywanie wierszy spe³niaj¹cych wiele warunków
--- Chcemy wyœwietliæ tylko te wiersze, które spe³niaj¹ kilka warunków
+--1.3. Odnajdywanie wierszy speÅ‚niajÄ…cych wiele warunkÃ³w
+-- Chcemy wyÅ›wietliÄ‡ tylko te wiersze, ktÃ³re speÅ‚niajÄ… kilka warunkÃ³w
 SELECT *
 FROM emp
 WHERE (deptno =10 OR comm IS NOT NULL OR sal <= 2000 )
 	AND deptno =20;
 
 --1.4. Odczytywanie podzbioru kolumn tabeli
--- Chcemy wyœwietliæ tylko okreœlone kolumny
+-- Chcemy wyÅ›wietliÄ‡ tylko okreÅ›lone kolumny
 SELECT ename, deptno, sal
 FROM emp;
 
 --1.5. Definiowanie sensownych nazw kolumn
--- Chcemy wyœwietliæ kolumny ze zmienionymi nazwami
+-- Chcemy wyÅ›wietliÄ‡ kolumny ze zmienionymi nazwami
 SELECT sal AS salary, comm AS commission
 FROM emp;
 
---1.6. Odwo³ania do aliasów kolumn w klauzuli WHERE
--- Chcemy odwo³aæ siê do kolumn reprezentowanych przez aliasy
+--1.6. OdwoÅ‚ania do aliasÃ³w kolumn w klauzuli WHERE
+-- Chcemy odwoÅ‚aÄ‡ siÄ™ do kolumn reprezentowanych przez aliasy
 SELECT *
 FROM (
 	SELECT sal AS salary, comm AS commission
 	FROM emp) x
 WHERE salary < 5000;
 
---1.7. Konkatenacja wartoœci kolumn
--- Chcemy zwróciæ wartoœci sk³adowane w wielu kolumnach w formie pojedynczej kolumny.
+--1.7. Konkatenacja wartoÅ›ci kolumn
+-- Chcemy zwrÃ³ciÄ‡ wartoÅ›ci skÅ‚adowane w wielu kolumnach w formie pojedynczej kolumny.
 SELECT ename + ' works as a ' + job AS msg
 FROM emp
 WHERE deptno = 10;
 
---1.8. Stosowanie logiki warunkowej w wyra¿eniu SELECT
--- Chcemy uzyskaæ zbiór wynikowy, w którym kolumna Status bêdzie zawiera³a wartoœæ Underpaid dla pracowników zarabiaj¹cych mniej ni¿ 2000, wartoœæ Overpaid dla pracowników
--- zarabiaj¹cych powy¿ej 4000 lub wartoœæ OK dla pracowników, których zarobki mieszcz¹ siê pomiêdzy 2000 a 4000
+--1.8. Stosowanie logiki warunkowej w wyraÅ¼eniu SELECT
+-- Chcemy uzyskaÄ‡ zbiÃ³r wynikowy, w ktÃ³rym kolumna Status bÄ™dzie zawieraÅ‚a wartoÅ›Ä‡ Underpaid dla pracownikÃ³w zarabiajÄ…cych mniej niÅ¼ 2000, wartoÅ›Ä‡ Overpaid dla
+--  pracownikÃ³w zarabiajÄ…cych powyÅ¼ej 4000 lub wartoÅ›Ä‡ OK dla pracownikÃ³w, ktÃ³rych zarobki mieszczÄ… siÄ™ pomiÄ™dzy 2000 a 4000
 SELECT ename, sal,
 	CASE WHEN sal <= 2000 THEN 'Underpaid'
 		WHEN sal >= 4000 THEN 'Overpaid'
@@ -56,24 +56,24 @@ SELECT ename, sal,
 FROM emp;
 
 --1.9. Ograniczanie liczby zwracanych wierszy
--- Chcemy ograniczyæ liczbê wyœmietlanych wierszy
+-- Chcemy ograniczyÄ‡ liczbÄ™ wyÅ›mietlanych wierszy
 SELECT TOP 5 *
 FROM emp;
 
---1.10. Zwracanie n losowych rekordów tabeli
--- Chcemy zwróciæ okreœlon¹ liczbê losowo wybranych rekordów
+--1.10. Zwracanie n losowych rekordÃ³w tabeli
+-- Chcemy zwrÃ³ciÄ‡ okreÅ›lonÄ… liczbÄ™ losowo wybranych rekordÃ³w
 SELECT TOP 5 ename, job
 FROM emp
 ORDER BY newid();
 
---1.11. Odnajdywanie wartoœci pustych (NULL)
--- Chcemy odnaleŸæ wszystkie wiersze, które zawieraj¹ puste wartoœci w okreœlonej kolumnie
+--1.11. Odnajdywanie wartoÅ›ci pustych (NULL)
+-- Chcemy odnaleÅºÄ‡ wszystkie wiersze, ktÃ³re zawierajÄ… puste wartoÅ›ci w okreÅ›lonej kolumnie
 SELECT *
 FROM emp
 WHERE comm IS NULL;
 
---1.12. Przekszta³canie wartoœci pustych w rzeczywiste
--- Chcemy aby w miejsce pustej wartoœci zwracane by³y inne wartoœci
+--1.12. PrzeksztaÅ‚canie wartoÅ›ci pustych w rzeczywiste
+-- Chcemy aby w miejsce pustej wartoÅ›ci zwracane byÅ‚y inne wartoÅ›ci
 SELECT COALESCE(comm, 0)
 FROM emp;
 
@@ -84,9 +84,9 @@ SELECT CASE
 	END
 FROM emp;
 
---1.13. Poszukiwanie wzorców
--- Chcemy aby zbiór wynikowy zawiera³ nazwiska i stanowiska pracy tylko tych pracowników dziesi¹tego i dwudziestego dzia³u, których nazwiska zawieraj¹ literê I lub których
--- nazwa stanowiska pracy koñczy siê liretami ER
+--1.13. Poszukiwanie wzorcÃ³w
+-- Chcemy aby zbiÃ³r wynikowy zawieraÅ‚ nazwiska i stanowiska pracy tylko tych pracownikÃ³w dziesiÄ…tego i dwudziestego dziaÅ‚u, ktÃ³rych nazwiska zawierajÄ… literÄ™ I lub 
+-- ktÃ³rych nazwa stanowiska pracy koÅ„czy siÄ™ liretami ER
 SELECT ename, job
 FROM emp
 WHERE deptno IN (10, 20)
