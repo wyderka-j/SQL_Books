02ef72778b3d0aa83871767225a6c5367ea63af4
wyderka-j <131911420+wyderka-j@users.noreply.github.com>
2024-09-14 13:04:45 +0200

    Update Zadania-Exercise.sql

--- A_Beaulieu-Wprowadzenie_do_SQL-Learning_SQL/Zadania-Exercise.sql
+++ A_Beaulieu-Wprowadzenie_do_SQL-Learning_SQL/Zadania-Exercise.sql
@@ -245,3 +245,61 @@ SELECT customer_id, COUNT(*), SUM(amount)
 FROM payment
 GROUP BY customer_id
 HAVING count(*) >=40;
+
+------------------------------
+-- Rozdział 9 / Chapter 9
+------------------------------
+
+-- Zadanie / Exercise 9.1
+-- Utwórz zapytanie do tabeli film wykorzystujące warunek filtrowania z niepowiązanym podzapytaniem do tabeli category w celu znalezienia wszystkich filmów akcji / 
+-- Construct a query against the film table that uses a filter condition with a noncorrelated subquery against the category table to find all action films
+-- (category.name = 'Action')
+
+SELECT f.title
+FROM film f 
+WHERE f.film_id IN (
+    SELECT fc.film_id
+    FROM film_category fc INNER JOIN category c
+  	 ON fc.category_id = c.category_id
+        WHERE c.name = 'Action');
+
+-- Zadanie / Exercise 9.2
+-- Zapytanie z ćwiczenia 9.1 zmodyfikuj w taki sposób, aby używało powiązanego podzapytania do tabel category i film_category w celu wygenerowania tych samych danych. /
+-- Rework the query from Exercise 9-1 using a correlated subquery against the category and film_category tables to achieve the same results.
+
+SELECT f.title
+FROM film f 
+WHERE EXISTS
+(SELECT 1
+FROM film_category fc INNER JOIN category c
+ON fc.category_id = c.category_id
+WHERE c.name = 'Action'
+AND fc.film_id = f.film_id);
+
+-- Zadanie / Exercise 9.3
+-- Przeprowadź złączenie następującego zapytania z podzapytaniem do tabeli film_actor w celu wyświetlenia informacji o poziomie poszczególnych aktorów:
+-- Join the following query to a subquery against the film_actor table to show the level of each actor:
+-- 			SELECT 'Hollywood Star' level, 30 min_roles, 99999 max_roles
+-- 			UNION ALL
+-- 			SELECT 'Prolific Actor' level, 20 min_roles, 29 max_roles
+-- 			UNION ALL
+-- 			SELECT 'Newcomer' level, 1 min_roles, 19 max_roles
+-- Podzapytanie do tabeli film_actor powinno zliczać rekordy dla każdego aktora za pomocą GROPU BY actor_id, a następnie ta wartość powinna zostać porównana z kolumnami 
+-- min_roles i max_roles w celu ustalenia poziomu danego aktora.
+-- A subquery to the film_actor table should count the records for each actor using GROUP BY actor_id, and then this value should be compared to the min_roles and max_roles
+-- columns to determine the actor's level.
+
+SELECT actr.actor_id, grps.level
+FROM
+(SELECT actor_id, count(*) num_roles
+FROM film_actor
+GROUP BY actor_id
+) actr
+INNER JOIN
+(SELECT 'Hollywood Star' level, 30 min_roles, 99999 max_roles
+UNION ALL
+SELECT 'Prolific Actor' level, 20 min_roles, 29 max_roles
+UNION ALL
+SELECT 'Newcomer' level, 1 min_roles, 19 max_roles
+) grps
+ON actr.num_roles BETWEEN grps.min_roles AND grps.max_roles;
